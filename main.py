be6f04f244fb9c6ae0d3bf7008f902aa092b890b
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2022-05-07 15:57:28 -0500

    Adding post endpoints

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def root():
             "status": 200}
 
 
-@app.post("/add-book/", response_model=SchemaBook)
+@app.post("/add-book", response_model=SchemaBook)
 async def add_book(book: SchemaBook):
     db_book = Book(title=book.title, rating=book.rating, author_id=book.author_id)
     db.session.add(db_book)
@@ -33,9 +33,15 @@ async def add_book(book: SchemaBook):
     return db_book
 
 
-@app.post("/add-author/", response_model=SchemaAuthor)
+@app.post("/add-author", response_model=SchemaAuthor)
 async def add_author(author: SchemaAuthor):
     db_author = Author(name=author.name, age=author.age)
     db.session.add(db_author)
     db.session.commit()
     return db_author
+
+
+@app.get("/books")
+def get_books():
+    books = db.session.query(Book).all()
+    return books
